cb131ca0405254e59784333db96e138efa448c84
KlunkyKong <danielhjermitslev@protonmail.com>
2020-12-08 23:34:04 +0100

    learn 2 route

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express')
 var app = express();
 
+// To create an endpoint that looks something like this: https://localhost:1337/api/v1/whatever
+/* app.get("/api/v1/cheeses", function (req, res) {
+}) */
+
+app.get("/hello", function (request, response) {
+    response.send("Hello, Dweller!");
+})
+
+app.get("/goodbye", function (request, response) {
+    response.send("Goodbye, Dweller see you next time!");
+})
 
 
 app.listen(1337, function () {
@@ -9,3 +20,4 @@ app.listen(1337, function () {
 
 
 // HTTP requests must get a response.
+// POST GET PUT/PATCH DELETE
