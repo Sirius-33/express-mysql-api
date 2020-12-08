d7b2fc7455cdb4efcf1da1f1c0b72d27a1e3d838
KlunkyKong <danielhjermitslev@protonmail.com>
2020-12-09 00:06:10 +0100

    add formidable

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 const express = require('express')
 var app = express();
+var formidable = require('formidable'); // package to make express understand data from forms
 
 // To create an endpoint that looks something like this: https://localhost:1337/api/v1/whatever
 /* app.get("/api/v1/cheeses", function (req, res) {
 }) */
 
+app.use(formidable);
+
+// Routing: website.com/hello - goodbye
 app.get("/hello", function (request, response) {
     response.send("Hello, Dweller!");
-})
+});
 
 app.get("/goodbye", function (request, response) {
     response.send("Goodbye, Dweller see you next time!");
+});
+
+// We can also add query strings to the routes:
+app.get("/hello/:name", function (request, response) {
+    response.send("Hello, " + request.params.name + "!");
+});
+
+// Create a thing on the server:
+app.post("/hello", function (request, response) {
+    console.log(request.fields);
+    response.end(); // sends a response back to the client, as we always need a response
 })
 
-
+// Our server
 app.listen(1337, function () {
     console.log("Server started");
 });
