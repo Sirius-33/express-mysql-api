f1fb929434cfaa21f4bbc9d6e65ea54625b3f8b8
KlunkyKong <danielhjermitslev@protonmail.com>
2020-12-10 09:24:12 +0100

    add callback

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ app.use(formidable());
 
 require("./cheeses_route")(app);
 
+app.use(function(error, request, response) {
+    if (error) {
+        console.log(error);
+        response.status(500);
+        response.json({
+            statusCode: 500,
+            statusText: "InternalServer error"
+        });
+    }
+});
+
 app.listen(1337, function() {
     console.log("Server started");
 });
--- cheeses_route.js
+++ cheeses_route.js
@@ -2,12 +2,12 @@ var connection = require("./database");
 
 module.exports = function (app) {
     // Create a cheese
-    app.post("/api/v1/cheeses", function (request, response) {
+    app.post("/api/v1/cheeses", function (request, response, next) {
         var sql = `INSERT INTO cheeses
 							SET name = :name, price = :price, weight = :weight,
 							strength = (SELECT strengths.id FROM strengths WHERE name = :strength),
                             brand = (SELECT brands.id FROM brands WHERE name = :brand)`;
-                            
+
         /* var sql = `INSERT INTO cheeses
         (name, price, weight, strength, brand)
         values (:name, :price, :weight, :strength, :brand)`; */
@@ -21,37 +21,21 @@ module.exports = function (app) {
         };
 
         connection.execute(sql, placeholders, function (err, result) {
-            if (err) {
-                console.log(err);
-                response.status(500);
-                response.json({
-                    statusCode: 500,
-                    statusText: "Internal server error"
-                });
-                return;
-            }
+            if (err) return next(err);
 
             // result.insertId
             //response.json(result); // this is not the final form
             var sql = `SELECT * FROM cheeses WHERE id = :id`;
             var placeholders = { id: result.insertId };
             connection.execute(sql, placeholders, function (err, result) {
-                if (err) {
-                    console.log(err);
-                    response.status(500);
-                    response.json({
-                        statusCode: 500,
-                        statusText: "Internal server error"
-                    });
-                    return;
-                }
+                if (err) return next(err);
                 response.json(result);
             });
         });
     });
 
     // GET all cheeses
-    app.get("/api/v1/cheeses", function (request, response) {
+    app.get("/api/v1/cheeses", function (request, response, next) {
         var sql = `SELECT cheeses.id, cheeses.name, cheeses.price, cheeses.weight,
 							 brands.name AS brand, strengths.name AS strength FROM cheeses
 							 INNER JOIN brands
@@ -60,22 +44,14 @@ module.exports = function (app) {
 								 ON strengths.id = cheeses.strength`;
 
         connection.query(sql, function (err, result) {
-            if (err) {
-                console.log(err);
-                response.status(500);
-                response.json({
-                    statusCode: 500,
-                    statusText: "Internal server error"
-                });
-                return;
-            }
+            if (err) return next(err);
 
             response.json(result);
         });
     });
 
     // GET a single cheese from ID
-    app.get("/api/v1/cheeses/:id", function (request, response) {
+    app.get("/api/v1/cheeses/:id", function (request, response, next) {
         var sql = `SELECT cheeses.id, cheeses.name, cheeses.price, cheeses.weight,
 							 brands.name AS brand, strengths.name AS strength FROM cheeses
 							 INNER JOIN brands
@@ -87,15 +63,7 @@ module.exports = function (app) {
         var placeholders = { id: request.params.id };
 
         connection.query(sql, placeholders, function (err, result) {
-            if (err) {
-                console.log(err);
-                response.status(500);
-                response.json({
-                    statusCode: 500,
-                    statusText: "Internal server error"
-                });
-                return;
-            }
+            if (err) return next(err);
 
             if (!result.length) {
                 response.status(404);
@@ -108,24 +76,17 @@ module.exports = function (app) {
     });
 
     // DELETE a cheese from ID
-    app.delete("api/v1/cheeses/:id", function (request, response) {
+    app.delete("api/v1/cheeses/:id", function (request, response, next) {
         var sql = `DELETE FROM cheese WHERE id = :id`;
         var placeholders = { id: request.params.id };
 
         connection.execute(sql, placeholders, function (err, result) {
-            if (err) {
-                console.log(err);
-                response.status(500);
-                response.json({
-                    statusCode: 500,
-                    statusText: "InternalServer error"
-                });
-                return
-            }
+            if (err) return next(err);
             // result.affectedRows
             if (result.affectedRows != 1) {
-                response.status(400);
+                response.status(404);
                 response.end();
+                return;
             }
 
             response.status(204);
@@ -134,7 +95,37 @@ module.exports = function (app) {
     });
 
     // UPDATE/patch A Single Cheese
-    app.patch("/api/v1/cheeses/:id", function(request, response) {
-        
-    })
+    app.patch("/api/v1/cheeses/:id", function (request, response, next) {
+        var sql = `UPDATE cheeses
+                        SET NAME = :name, price = :price, weight = :weight,
+                        strength = :strength, brand = :brand
+                        WHERE id = :id`;
+
+        var placeholders = {
+            name: request.fields.name, 
+            price: request.fields.price,
+            weight: request.fields.weight,
+            strength: request.fields.strength,
+            brand: request.fields.brand, // fields is related to our SQL table containing this "field"
+            id: request.params.id // params is referenced in the URL 
+        };
+
+        connection.execute(sql, placeholders, function (err, result) {
+            if (err) return next(err);
+            // result.affectedRows
+            if (result.affectedRows != 1) {
+                response.status(404);
+                response.end();
+                return;
+            }
+
+            var sql = `SELECT * FROM cheeses WHERE id = :id`;
+            var placeholders = { id: request.params.id };
+            connection.execute(sql, placeholders, function (err, result) {
+                if (err) return next(err);
+
+                response.json(result[0]);
+            });
+        });
+    });
 };
